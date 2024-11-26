d5cb6d6d94a02ba145d8dbdccc2cfe7e33cb39fe
shubham6822 <sharmashubham6822@gmail.com>
2024-11-26 15:14:31 +0530

    button added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,6 @@
 import { useRef, useState } from 'react'
+import { TiLocationArrow } from "react-icons/ti";
+import Button from './Button';
 
 export default function Hero() {
 
@@ -67,19 +69,18 @@ export default function Hero() {
                 <div className="absolute left-0 top-0 z-40 size-full">
                     <div className="mt-24 px-5 sm:px-10">
                         <h1 className="special-font hero-heading text-blue-100">
-                            redefi<b>n</b>e
-                        </h1>
+                            LU<b>K</b>O                        </h1>
 
                         <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
                             Enter the Metagame Layer <br /> Unleash the Play Economy
                         </p>
 
-                        {/* <Button
-              id="watch-trailer"
-              title="Watch trailer"
-              leftIcon={<TiLocationArrow />}
-              containerClass="bg-yellow-300 flex-center gap-1"
-            /> */}
+                        <Button
+                            id="watch-trailer"
+                            title="Play"
+                            leftIcon={<TiLocationArrow />}
+                            containerClass="bg-yellow-300 flex-center gap-1"
+                        />
                     </div>
                 </div>
             </div>
