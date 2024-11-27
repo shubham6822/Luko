3f8cda7122a8d7236d23e1aa0899eccbb2ef3c51
shubham6822 <sharmashubham6822@gmail.com>
2024-11-27 12:06:08 +0530

    feature section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
+import Feature from './components/Feature'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <Hero />
       <About />
+      <Feature />
     </div>
   )
 }
